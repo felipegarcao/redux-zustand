ac65c984d2a8ff68e4462adc2b3546708ec095db
Luis Felipe Garção <felipe-mara2003@hotmail.com>
2023-07-20 09:36:53 -0300

    Disparando actions no Redux

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,12 +1,22 @@
 import { FormEvent, useState } from "react"
+import { useDispatch } from 'react-redux'
+import { add } from '../store'
 
 export function AddTodo() {
   const [newTodo, setNewTodo] = useState('')
+  const dispatch = useDispatch()
 
   function handleNewTodo(e: FormEvent) {
     e.preventDefault()
 
-    console.log(newTodo)
+    if (newTodo.length > 2) {
+      dispatch(add({
+        newTodo
+      }))
+    }
+   
+
+    setNewTodo('')
   }
 
   return (
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,17 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit';
+import { TypedUseSelectorHook, useSelector } from 'react-redux';
 
 
 
 const todoSlice = createSlice({
   name: 'todo',
   initialState: ['Fazer café', 'Estudar Redux', 'Ir ao medico'],
-  reducers: {}
+  reducers: {
+    add: (state, action) => {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
+      state.push(action.payload.newTodo)
+    }
+  }
 })
 
 export const store = configureStore({
@@ -13,3 +19,8 @@ export const store = configureStore({
     todo: todoSlice.reducer
   }
 })
+
+export const {add} = todoSlice.actions
+
+export type RootState =  ReturnType<typeof store.getState>
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
