f70aec8e960b461239bc6adc21dd14cb60ba1caa
Luis Felipe Garção <felipe-mara2003@hotmail.com>
2023-07-20 10:39:41 -0300

    Separando componentes

---
+++ src/components/Module.tsx
@@ -0,0 +1,34 @@
+import { ChevronDown } from 'lucide-react';
+import { Lesson } from './Lesson';
+
+
+interface ModuleProps {
+  moduleIndex: number;
+  title: string;
+  amountOfLessons: number
+}
+
+export function Module({title, amountOfLessons, moduleIndex }: ModuleProps) {
+  return (
+    <div>
+      <button className='flex w-full items-center gap-3 bg-zinc-800 p-4'>
+        <div className='flex h-10 w-10 rounded-full items-center justify-center bg-zinc-950 text-xs'>
+          {moduleIndex + 1}
+        </div>
+
+        <div className='flex flex-col gap-1 text-left'>
+          <strong className='text-sm'>{title}</strong>
+          <span className="text-xs text-zinc-400">{amountOfLessons} aulas</span>
+        </div>
+
+        <ChevronDown className='w-5 h-5 ml-auto text-zinc-400' />
+      </button>
+
+      <nav className='relative flex flex-col gap-4 p-6'>
+        <Lesson title='Fundamentos do Redux' duration='16:33' />
+        <Lesson title='Criando store do Redux' duration='12:17' />
+        <Lesson title='Disparando actions no Redux' duration='09:24' />
+      </nav>
+    </div>
+  )
+}
